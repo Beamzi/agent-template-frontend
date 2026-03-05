50507ec4733c13dc06f454f890b8d62875aaa4a4
james <159420270+Beamzi@users.noreply.github.com>
2026-03-05 21:12:11 +1100

    added button-gradient to styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Sulphur_Point } from "next/font/google";
 import "./globals.css";
+import "../styles/button-gradient.css";
 
 const geistSans = Sulphur_Point({
   variable: "--font-primary",
